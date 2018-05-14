6dc272e76aa5fdb4da17833092c55df64660044c
markm-at-014608743065 <web-developer@michellemark.me>
2018-05-14 17:00:46 -0400

    a few starter tweaks to the site nav

--- wiseone/static/js/wiseone-common.js
+++ wiseone/static/js/wiseone-common.js
@@ -8,9 +8,7 @@ let wise_one_js = (function() {
 
     return {
         setup_page: function () {
-            // Give site wide top navigation items with children arrows
-            $("#main_navigation li.child").has("ul").children("a").append(
-                "<i class='material-icons md-inactive align-bottom'>arrow_drop_down</i>");
+
         }
     };
 
