22e8e450f7f8609ab0de2204fa2d9233ff250386
markm-at-014608743065 <web-developer@michellemark.me>
2018-05-30 17:33:30 -0400

    fleshing out start to about, buyers, seller, top nav

--- public_pages/urls.py
+++ public_pages/urls.py
@@ -3,9 +3,12 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from django.conf.urls import url
 
-from public_pages.views import HomeView, ComingSoonView
+from public_pages.views import HomeView, ComingSoonView, AboutWiseOneView, BuyersInfoView, SellersInfoView
 
 urlpatterns = [
+    url(r'^home-sellers-info/', SellersInfoView.as_view(), name='sellersinfo'),
+    url(r'^home-buyer/', BuyersInfoView.as_view(), name='buyersinfo'),
+    url(r'^about/', AboutWiseOneView.as_view(), name='aboutus'),
     url(r'^home/', HomeView.as_view(), name='home'),
     url(r'^', ComingSoonView.as_view(), name='comingsoon'),
 ]
--- public_pages/views.py
+++ public_pages/views.py
@@ -22,3 +22,33 @@ class HomeView(TemplateView):
         context['object'] = HomeFeaturedArticlePlaceholder.objects.get_or_create(pk=1)[0]
 
         return context
+
+
+class AboutWiseOneView(TemplateView):
+    template_name = 'about-us.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AboutWiseOneView, self).get_context_data()
+        context['page_title'] = "About Wise One Realty"
+
+        return context
+
+
+class BuyersInfoView(TemplateView):
+    template_name = 'buyers-info.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(BuyersInfoView, self).get_context_data()
+        context['page_title'] = "Buy a Home in CNY with Wise One Realty"
+
+        return context
+
+
+class SellersInfoView(TemplateView):
+    template_name = 'sellers-info.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SellersInfoView, self).get_context_data()
+        context['page_title'] = "Sell my CNY Home with Wise One Realty"
+
+        return context
